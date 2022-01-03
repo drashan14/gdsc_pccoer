d8c75fb510f991d016919df3491b54b670e6e5e1
Sneha <snehazambare16@gmail.com>
2022-01-03 22:20:05 +0530

    msg

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from "@chakra-ui/react";
 import GlobalStyles from './styles/GlobalStyles';
 import Typography from './styles/Typography';
@@ -16,4 +16,4 @@ ReactDOM.render(
     document.getElementById('root')
     );
 
-reportWebVitals(console.log);
+// reportWebVitals(console.log);
